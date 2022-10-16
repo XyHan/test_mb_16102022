13bc8fdf5a9764ac6284f74f1adac1a4fcf925d0
Robin CHEVALIER <chevalier.robin@yahoo.fr>
2022-10-16 12:11:18 +0200

    feat(enum): add CustomField types enum

---
+++ src/MobilityWork/Enum/CustomFieldTypeEnum.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace MobilityWork\Enum;
+
+enum CustomFieldTypeEnum: string
+{
+    case Customer = 'customer';
+    case Hotel = 'hotel';
+    case Press = 'press';
+    case Partner = 'partner';
+}
